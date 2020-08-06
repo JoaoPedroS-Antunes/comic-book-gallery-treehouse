5da889b2b5d6489a9e711e3d9a66bbd65c6105b9
byter <joaoantunes76@protonmail.com>
2020-08-06 13:11:51 +0100

    added a Controller

---
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
