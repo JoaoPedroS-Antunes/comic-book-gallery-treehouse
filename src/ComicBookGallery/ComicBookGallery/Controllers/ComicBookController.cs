703a149ea88b1c65f85d80ea1ce23171185696d0
John Master <byter@outlook.pt>
2020-08-11 13:37:15 +0100

    Added Comic Book Detail View

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
@@ -10,14 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return new RedirectResult("/");
-            }
-            return new ContentResult()
-            {
-                Content = "Hello from the comic book controller!"
-            };
+            return View();
         }
     }
 }
