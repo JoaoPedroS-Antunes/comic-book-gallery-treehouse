c684c4ea108ba67c79504137c60a441bdb3506c5
John Master <byter@outlook.pt>
2020-08-11 17:52:03 +0100

    Updated the default route

--- src/ComicBookGallery/ComicBookGallery/Startup.cs
+++ src/ComicBookGallery/ComicBookGallery/Startup.cs
@@ -35,7 +35,7 @@ namespace ComicBookGallery
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/ComicBook/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -49,8 +49,8 @@ namespace ComicBookGallery
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "default",                   
+                    pattern: "{controller=ComicBook}/{action=Index}/{id?}");
             });
         }
     }
